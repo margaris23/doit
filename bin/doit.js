fd9aa2c3487daf7a3aeede2c7606b742e835d44e
Nikolaos Margaris <reverence23@gmail.com>
2014-10-30 00:11:46 +0200

    Added more default folders & files

--- bin/doit.js
+++ bin/doit.js
@@ -2,18 +2,23 @@
 
 var fs = require('fs');
 
+// make sure current working directory is used
 var cwd = process.cwd();
+
+// use rest of arguments provided as input
 var args = process.argv.slice(2);
 
 function error(msg) {
   console.error('Error: ' + msg);
 }
 
+// validate # args
 if (args.length < 3) {
   error('wrong number of arguments!');
   return -1;
 }
 
+// define working product for scaffolding
 var product = args[0];
 
 if (product !== 'angular') {
@@ -21,9 +26,12 @@ if (product !== 'angular') {
   return;
 }
 
-function handleSrc(folder, content) {
+// create src files in folder with provided content
+function handleSrc(folder, file, content) {
 	// always use relative paths
 	var innerPath = cwd + '/src/' + folder;
+
+	// makesure configuration file exists
   fs.readFile(cwd + '/project.conf', function(err) {
 		if (err) {
       error(err);
@@ -31,11 +39,13 @@ function handleSrc(folder, content) {
       return;
     }
 
+		// check if folder exists first
 		if (!fs.existsSync(innerPath)) {
 			fs.makedirSync(innerPath);
 		}
 
-		fs.writeFile(innerPath+ '/' + args[2], content, function (err) {
+		// create file with provided content
+		fs.writeFile(innerPath + '/' + file, content, function (err) {
 			if (err) {
 				error(err);
 				return;
@@ -45,35 +55,61 @@ function handleSrc(folder, content) {
 	});
 }
 
-var mode = args[1];
+var action = args[1];
 
-switch(mode) {
+switch(action) {
   case 'init':
+		// create top folder
     fs.mkdir(args[2], function (err) {
       if (err) {
         error(err);
         return;
       }
+
+			// create configuration file
       fs.writeFileSync(args[2] + '/project.conf', 'project.name =' + args[2]);
-      fs.mkdirSync(args[2] + '/src');
+
+			// create srcfolders
+			var srcFolder = args[2] + '/src/';
+      fs.mkdirSync(srcFolder);
+      fs.mkdirSync(srcFolder + 'services');
+			fs.writeFileSync(srcFolder + 'services/' + args[2] + 'Services.js',
+											 'var services = angular.module("' + args[2] + '.services", []);\n');
+      fs.mkdirSync(srcFolder + 'controllers');
+			fs.writeFileSync(srcFolder + 'controllers/' + args[2] + 'Controllers.js',
+											 'var controllers = angular.module("' + args[2] + '.controllers", []);\n');
+      fs.mkdirSync(srcFolder + 'directives');
+			fs.writeFileSync(srcFolder + 'directives/' + args[2] + 'Directives.js',
+											 'var directives = angular.module("' + args[2] + '.directives", []);\n');
+      fs.mkdirSync(srcFolder + 'filters');
+			fs.writeFileSync(srcFolder + 'filters/' + args[2] + 'Filters.js',
+											 'var filters= angular.module("' + args[2] + '.filters", []);\n');
+      fs.mkdirSync(srcFolder + 'common');
+
+			// create other folders
+      fs.mkdirSync(args[2] + '/views');
+      fs.mkdirSync(args[2] + '/lib');
     });
     break;
   case 'create-service':
-    handleSrc('services', '//angular service');
+    handleSrc('services', args[2], '//angular service');
     break;
   case 'create-controller':
-		handleSrc('controllers', '//angular controller');
+		handleSrc('controllers', args[2], '//angular controller');
     break;
   case 'create-view':
 		// TBD
     break;
   case 'create-common':
-		handleSrc('common', '//common object');
+		handleSrc('common', args[2], '//common object');
     break;
   case 'create-directive':
-		handleSrc('directives', '//angular directive');
+		handleSrc('directives', args[2], '//angular directive');
     break;
-  default:
+  case 'create-filter':
+		handleSrc('filters', args[2], '//angular filter');
+    break;
+	default:
     error('uknown action');
     return;
 }
